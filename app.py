b5c73dc105ed8b212db8792970947500f4b9eb3b
Veeline <veeline.ho@gmail.com>
2025-08-08 16:13:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -44,7 +44,10 @@ st.markdown(
 uid = "vmlitagm"
 pwd = "Manish@25"
 
-loginpwd ="Vil@2024"
+mpwd="Vil@2024"
+sik="Sik@121"
+sur="Sur@211"
+nal="Nal@122"
 
 
 current_date = datetime.now().strftime("%Y-%m-%d")
@@ -69,69 +72,144 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
+scan="http://52.140.78.30:10108/VILUAT_29378/ODataV4/Company('Veeline%20Industries%20Limited')/FinalStageScanning?$filter=Date eq {}" .format(current_date)
+dpr="http://52.140.78.30:10108/VILUAT_29378/ODataV4/Company('Veeline%20Industries%20Limited')/ItemLedgerEntires?$filter=Posting_Date ge {}" .format(current_date)
+resp1 = requests.get(scan,auth=HTTPBasicAuth(uid,pwd))
+resp2 = requests.get(dpr,auth=HTTPBasicAuth(uid,pwd))
 
-Password = st.text_input("Enter the Password",type='password')
-if Password==loginpwd:
-        if st.button("Refresh"):
+def report(branch):
+    resp1 = requests.get(scan,auth=HTTPBasicAuth(uid,pwd))
+    resp2 = requests.get(dpr,auth=HTTPBasicAuth(uid,pwd))
+    data = resp1.json()["value"]
+    data2 = resp2.json()["value"]
+    scandf=pd.DataFrame(data)
+    dprdf=pd.DataFrame(data2)
+    if branch:
+        scandf = scandf[scandf["Branch"] == branch][["Line_No","Date","Branch","Division"]]
+        dprdf=dprdf[(dprdf["Entry_Type"].str.startswith("Output")) & (dprdf["Source_No"].str.startswith("FG")) & (dprdf["Global_Dimension_1_Code"]==branch)][["Posting_Date","Global_Dimension_1_Code","Global_Dimension_2_Code","Quantity"]]
+        dprdf["key"]=dprdf["Posting_Date"].astype(str)+"/"+dprdf["Global_Dimension_1_Code"]+"/"+dprdf["Global_Dimension_2_Code"]
+        scandf["key"]=scandf["Date"].astype(str)+"/"+scandf["Branch"]+"/"+scandf["Division"] 
+        scan_data = scandf.groupby("key")["Line_No"].count()
+        output_data = dprdf.groupby("key")["Quantity"].sum()
+        result = pd.merge(scan_data,output_data,how="left",on="key")
+        result["Quantity"]=result["Quantity"].fillna(0)
+        result=result.reset_index()
+        result["Date"]=result["key"].str.split("/").apply(lambda x: x[0])
+        result["Branch"]=result["key"].str.split("/").apply(lambda x: x[1])
+        result["Division"]=result["key"].str.split("/").apply(lambda x: x[2])
+        result.rename(columns={"Line_No":"Scan","Quantity":"DPR"},inplace=True)
+        result = result[result["Branch"]!=""]
+        # Melt the DataFrame to long format
+        df_melted = result.melt(
+        id_vars=["Branch", "Division", "Date"],
+        value_vars=["Scan", "DPR"],
+        var_name="Metric",
+        value_name="Value"
+            )
+        # Pivot to reshape with Line_No and Quantity side-by-side for each Date
+        pivot = df_melted.pivot_table(
+        index=["Branch", "Division"],
+        columns=["Date", "Metric"],
+        values="Value",
+        aggfunc="sum",
+        fill_value=0
+            )
+        # Flatten the multi-level column index (optional but cleaner)
+        pivot.columns = [f"{date} {metric}" for date, metric in pivot.columns]
+        pivot = pivot.reset_index()
+        st.write(pivot)
+    else:
+        scandf = scandf[["Line_No","Date","Branch","Division"]]
+        dprdf=dprdf[(dprdf["Entry_Type"].str.startswith("Output")) & (dprdf["Source_No"].str.startswith("FG"))][["Posting_Date","Global_Dimension_1_Code","Global_Dimension_2_Code","Quantity"]]
+        dprdf["key"]=dprdf["Posting_Date"].astype(str)+"/"+dprdf["Global_Dimension_1_Code"]+"/"+dprdf["Global_Dimension_2_Code"]
+        scandf["key"]=scandf["Date"].astype(str)+"/"+scandf["Branch"]+"/"+scandf["Division"] 
+        scan_data = scandf.groupby("key")["Line_No"].count()
+        output_data = dprdf.groupby("key")["Quantity"].sum()
+        result = pd.merge(scan_data,output_data,how="left",on="key")
+        result["Quantity"]=result["Quantity"].fillna(0)
+        result=result.reset_index()
+        result["Date"]=result["key"].str.split("/").apply(lambda x: x[0])
+        result["Branch"]=result["key"].str.split("/").apply(lambda x: x[1])
+        result["Division"]=result["key"].str.split("/").apply(lambda x: x[2])
+        result.rename(columns={"Line_No":"Scan","Quantity":"DPR"},inplace=True)
+        result = result[result["Branch"]!=""]
+        # Melt the DataFrame to long format
+        df_melted = result.melt(
+        id_vars=["Branch", "Division", "Date"],
+        value_vars=["Scan", "DPR"],
+        var_name="Metric",
+        value_name="Value"
+            )
+        # Pivot to reshape with Line_No and Quantity side-by-side for each Date
+        pivot = df_melted.pivot_table(
+        index=["Branch", "Division"],
+        columns=["Date", "Metric"],
+        values="Value",
+        aggfunc="sum",
+        fill_value=0
+            )
+        # Flatten the multi-level column index (optional but cleaner)
+        pivot.columns = [f"{date} {metric}" for date, metric in pivot.columns]
+        pivot = pivot.reset_index()
+        st.write(pivot)
 
-            scan="http://52.140.78.30:10108/VILUAT_29378/ODataV4/Company('Veeline%20Industries%20Limited')/FinalStageScanning?$filter=Date eq {}" .format(current_date)
-            dpr="http://52.140.78.30:10108/VILUAT_29378/ODataV4/Company('Veeline%20Industries%20Limited')/ItemLedgerEntires?$filter=Posting_Date ge {}" .format(current_date)
-
-            resp1 = requests.get(scan,auth=HTTPBasicAuth(uid,pwd))
-            if resp1.status_code==200:
-                data = resp1.json()["value"]
-                scandf=pd.DataFrame(data)
-                if scandf.shape[0]!=0:
-                    resp2 = requests.get(dpr,auth=HTTPBasicAuth(uid,pwd))
-                    if resp2.status_code==200:
-                        data = resp2.json()["value"]
-                        dprdf=pd.DataFrame(data)
-                    else: 
-                        print(resp2.status_code)
-
-                    scandf=scandf[["Line_No","Date","Branch","Division"]]
-
-                    dprdf=dprdf[(dprdf["Entry_Type"].str.startswith("Output")) & (dprdf["Source_No"].str.startswith("FG"))][["Posting_Date","Global_Dimension_1_Code","Global_Dimension_2_Code","Quantity"]]
-
-                    dprdf["key"]=dprdf["Posting_Date"].astype(str)+"/"+dprdf["Global_Dimension_1_Code"]+"/"+dprdf["Global_Dimension_2_Code"]
-                    scandf["key"]=scandf["Date"].astype(str)+"/"+scandf["Branch"]+"/"+scandf["Division"] 
-
-                    scan_data = scandf.groupby("key")["Line_No"].count()
-                    output_data = dprdf.groupby("key")["Quantity"].sum()
-                    result = pd.merge(scan_data,output_data,how="left",on="key")
-                    result["Quantity"]=result["Quantity"].fillna(0)
-                    result=result.reset_index()
-
-                    result["Date"]=result["key"].str.split("/").apply(lambda x: x[0])
-                    result["Branch"]=result["key"].str.split("/").apply(lambda x: x[1])
-                    result["Division"]=result["key"].str.split("/").apply(lambda x: x[2])
-
-                    result.rename(columns={"Line_No":"Scan","Quantity":"DPR"},inplace=True)
-
-                    # Melt the DataFrame to long format
-                    df_melted = result.melt(
-                        id_vars=["Branch", "Division", "Date"],
-                        value_vars=["Scan", "DPR"],
-                        var_name="Metric",
-                        value_name="Value"
-                    )
-
-                    # Pivot to reshape with Line_No and Quantity side-by-side for each Date
-                    pivot = df_melted.pivot_table(
-                        index=["Branch", "Division"],
-                        columns=["Date", "Metric"],
-                        values="Value",
-                        aggfunc="sum",
-                        fill_value=0
-                    )
-
-                    # Flatten the multi-level column index (optional but cleaner)
-                    pivot.columns = [f"{date} {metric}" for date, metric in pivot.columns]
-
-
-                    pivot = pivot.reset_index()
-                    st.write(pivot)
-                else:
-                     st.write("No Scanning Data is available for Current Date")
+if resp1.status_code==200 and resp2.status_code==200: 
+    st.write("Online")
+    if pd.DataFrame(resp1).shape[0]!=0:
+        access=st.text_input("Enter the access password",type="password")
+        if access==mpwd:
+            if st.button("Refresh"):
+                report(branch=None)
+        elif access==sik:
+            if st.button("Refresh"):
+                report(branch="SIK")
+        elif access==sur:
+            if st.button("Refresh"):
+                report(branch="SUR")
+        elif access==nal:
+            if st.button("Refresh"):
+                report(branch="NAL")
+        else:
+            st.write("Enter a valid passkey")
+              
+    else:
+        st.write("No Scanning Data is available for Current Date")
 else:
-     st.write("Enter valid pin to access the report")
+    st.write("DataBase authentication error:")    
+         
+                 
+
+         
+
+
+
+
+
+
+
+
+         
+         
+         
+         
+    
+         
+
+
+
+
+
+     
+
+            
+
+            
+            
+                    
+
+                    
+
+                   
+                    
+
+                    
